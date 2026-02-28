68464edfdacae8c7a1ab888661b717eea0cdac40
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2026-02-28 19:58:58 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+var galleryDiv = document.getElementById('gallery');
+var timelineDiv = document.getElementById('timeline');
+var gamesDiv = document.getElementById('games');
+var hiddenDiv = document.getElementById('hiddenDiv');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // check for all 3 checks
+    if (sessionStorage.getItem('gallery') && sessionStorage.getItem('timeline') && sessionStorage.getItem('games')) {
+        hiddenDiv.style.display = 'flex';
+    }
+    [galleryDiv, timelineDiv, gamesDiv].forEach(div => {
+        div.addEventListener('click', () => {
+            sessionStorage.setItem(`${div.id}`, 'true');
+            console.log('Logged '+ div.id);
+            document.location.href = `pages/${div.id}.html`;
+        })
+    });
+    hiddenDiv.addEventListener('click', () => {
+        document.location.href = 'pages/secret.html';
+    })
+})
